f59924e0152f9fe59077aa97a453ad70650c3a4d
nemo2121 <123516217+nemo2121@users.noreply.github.com>
2023-01-25 04:19:22 +0500

    Update MyProject5-HTML5-Shipping.UE4.js

--- MyProject5-HTML5-Shipping.UE4.js
+++ MyProject5-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyProject5-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/nemo2121/test/97e2ba6c22312992897b7fd23bca4452ba87aa0e/MyProject5-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
